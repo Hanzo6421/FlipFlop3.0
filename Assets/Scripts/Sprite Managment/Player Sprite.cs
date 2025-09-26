7fbdaba7c919ed1b0787ee59dfcbf5274cf0a08a
Builderbosc <oscarjackdryden@gmail.com>
2025-09-26 14:10:14 +1000

    Player Sprite behaviour implemented and sfx
    Needs to flip facing direction when moving in a different direction/ roate in a different direction for 3D mode.

--- Assets/Scripts/Sprite Managment/Player Sprite.cs
+++ Assets/Scripts/Sprite Managment/Player Sprite.cs
@@ -1,19 +1,35 @@
+using FlipFlop.Gameplay;
 using UnityEngine;
 
 public class PlayerSprite : MonoBehaviour
 {
-
+    private PlayerCharacterController characterController;
     public Transform playerTransform;
+    public Animator playerSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        characterController = FindAnyObjectByType<PlayerCharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         transform.position = playerTransform.position;
+
+        if (!characterController.isGrounded)
+        {
+            playerSprite.Play("Magician_Anim_Jump");
+        }
+        else if (Vector3Int.RoundToInt(characterController.characterVelocity) != Vector3.zero) 
+        {
+            playerSprite.Play("Magician_Anim_Run");
+        }
+        else
+        {
+            playerSprite.Play("Magician_Anim_Idle");
+        }
     }
 }
